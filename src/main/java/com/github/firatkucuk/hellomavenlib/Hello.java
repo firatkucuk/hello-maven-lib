0932af10766a154eb06b0136589d89336b47db82
firatkucuk <firat@fria.io>
2020-05-31 17:06:20 +0200

    initial commit

---
+++ src/main/java/com/github/firatkucuk/hellomavenlib/Hello.java
@@ -0,0 +1,11 @@
+package com.github.firatkucuk.hellomavenlib;
+
+public class Hello {
+
+    private Hello() {
+    }
+
+    public static String sayHello(final String name) {
+        return "Hello " + name;
+    }
+}
